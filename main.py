1fc51ddbfd062209cb9c2c62a5822c6ec73b41d9
Kassio Wallace <kassiowl34@gmail.com>
2023-08-03 18:20:52 -0300

    print removed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from steps.search_page import search_page
 from steps.login_step import login_step
 from steps.send_connection import send_connection
 from steps.start_driver import start_driver
-from util.close_diver import close_driver
+from util.close_driver import close_driver
 
 
 credentials = get_credentials()
@@ -23,11 +23,6 @@ search_info = get_search_info()
 
 until_page = int(search_info.until_page)
 current_page = int(search_info.start_page)
-
-print("until page")
-print(until_page)
-print("current page")
-print(current_page)
 
 while current_page <= until_page:
     search_page(driver, current_page)
