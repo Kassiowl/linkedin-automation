e133bfa3fd2a103c01ff69a9e6e9672896cdc7cb
Kassio Wallace <kassiowl34@gmail.com>
2024-02-26 17:58:45 -0300

    adding pages in message feature

--- message_feature.py
+++ message_feature.py
@@ -1,6 +1,7 @@
 
 import time
 from repository.get_message import get_message
+from repository.get_search_string import get_search_info
 from steps.common_steps.login_step import login_step
 from steps.common_steps.start_driver import start_driver
 from steps.messages.go_to_send_message_page import go_to_send_message_main_page
@@ -14,10 +15,16 @@ message = get_message()
 print("Message")
 print(message.message_string)
 
+search_info = get_search_info()
+
+until_page = search_info.until_page
+current_page = search_info.start_page
+
 driver = start_driver()
-time.sleep(5)
 login_step(driver)
-time.sleep(5)
-go_to_send_message_main_page(driver)
-time.sleep(10)
-send_message(driver)
+while current_page <= until_page:
+    time.sleep(5)
+    go_to_send_message_main_page(driver, current_page)
+    time.sleep(5)
+    send_message(driver)
+    current_page+=1
