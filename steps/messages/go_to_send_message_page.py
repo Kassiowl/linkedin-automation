55399b6154950cf0a1cb50f11f395cba7489b5f7
Kassio Wallace <kassiowl34@gmail.com>
2024-02-26 18:12:06 -0300

    adding pages in message feature

--- steps/messages/go_to_send_message_page.py
+++ steps/messages/go_to_send_message_page.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 
 from repository.get_message import get_message
-
-def go_to_send_message_main_page(driver: webdriver):
+def go_to_send_message_main_page(driver: webdriver, page: int):
     message = get_message()
-    message_main_page = f'https://www.linkedin.com/search/results/people/?keywords={message.message_to_who}&network=%5B%22F%22%5D&origin=FACETED_SEARCH&sid=%3Bkx'
+    message_main_page = f'https://www.linkedin.com/search/results/people/?keywords={message.message_to_who}&network=%5B%22F%22%5D&origin=GLOBAL_SEARCH_HEADER&page={page}&sid=%2CLg'
     driver.get(message_main_page)
