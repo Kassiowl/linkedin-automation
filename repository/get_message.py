e99ea480aa78209f1c3067069520519b9b1bf582
Kassio Wallace <kassiowl34@gmail.com>
2024-02-26 16:40:44 -0300

    get_message mod, UTF-8 add

--- repository/get_message.py
+++ repository/get_message.py
@@ -6,6 +6,6 @@ MESSAGE_TEXT_PATH = 'static/message.txt'
 def get_message() -> Message:
     data = ''
     config = dotenv_values(".env")  
-    with open(MESSAGE_TEXT_PATH) as f:
+    with open(MESSAGE_TEXT_PATH, "r", encoding="UTF-8") as f:
         data = f.read()
     return Message(config['MESSAGE_TO_WHO'], data)
